6e20d58db078310cba9ae0caecebe9d2c89af77b
Jorge Blanchard Cruz <alu0100536591@ull.edu.es>
2014-05-16 20:07:50 +0100

    Cambio en ejemplo de formulario

--- main.rb
+++ main.rb
@@ -62,7 +62,7 @@ get '/:selected?' do |selected|
     \n\t\twidth 25
     \n\t\theight 25
     \n\n\tform\n\t\t
-    \n\tlbl \"Inicio\"
+    \n\t\"Inicio\"
     \n\ttable 2 30
     \n\t\ttxt \"etiqueta\" ident = \"valor por defecto\";
     \n\t\t;;
@@ -71,7 +71,7 @@ get '/:selected?' do |selected|
     \n\t\t;; 
     \n\t\tcbx idcombo = \"val1\" \"val2\" \"val3\" \"val4\"; 
     \n\tendtable;
-    \n\tlbl \"Fin\"
+    \n\t\"Fin\"
     \nend." end
   erb :index, 
       :locals => {  :programs => programs, :users => users, :source => source, 
