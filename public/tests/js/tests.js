de003806cf31bc5e40fbdc608da72dd024684dff
Gonzalo J. García Martín <=>
2014-05-17 13:38:58 +0100

    Pruebas AST cabecero sin y con opciones

--- public/tests/js/tests.js
+++ public/tests/js/tests.js
@@ -1,7 +1,19 @@
 var assert = chai.assert;
 
-suite('UNA PRUEBA', function() {
-	test('TODAVÍA NO DEFINIDA', function() {
-		
+suite('AST', function() {
+	test('Cabecero AST', function() {
+		var esperado = '[\n  {\n    "type": "HEAD",\n    "value": "Formulario"\n  },\n  {\n    "type": "FORM",\n    "value": []\n  }\n]';
+      var source = 'begin \n head Formulario \n form \n end.';
+      var result = ast.parse(source);
+      
+      assert.deepEqual (JSON.stringify(result,undefined,2), esperado);
 	});
+   
+   test('Cabecero con opciones AST', function() {
+      var esperado = '[\n  {\n    "type": "HEAD",\n    "value": "Formulario"\n  },\n  {\n    "type": "LOGO",\n    "value": "img/github.png"\n  },\n  {\n    "type": "WIDTH",\n    "value": 25\n  },\n  {\n    "type": "HEIGHT",\n    "value": 25\n  },\n  {\n    "type": "FORM",\n    "value": []\n  }\n]';
+      var source = 'begin head Formulario options logo "img/github.png" width 25 height 25 form end.';
+      var result = ast.parse(source);
+      
+      assert.deepEqual (JSON.stringify(result,undefined,2), esperado);
+   });   
 });
